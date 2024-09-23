1cbbaf00fa2920c2ca5df8969c6b4a8baeb72f6b
harsh <toraneharsh29@gmail.com>
2024-09-23 08:37:00 +0530

    template data

---
+++ header.php
@@ -0,0 +1,142 @@
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+    
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/owl-slider.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/settings.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/slick.css"/>
+        <link rel="shortcut icon" href="assets/images/favicon.png" />
+        <script type="text/javascript" src="assets/js/jquery-3.2.0.min.js"></script>
+        <title>Food Shop</title>
+    </head>
+    <body>
+    <div class="awe-page-loading">
+         <div class="awe-loading-wrapper">
+            <div class="awe-loading-icon">
+               <img src="assets/images/logo.png" alt="images">
+            </div>
+            <div class="progress">
+               <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+         </div>
+      </div> 
+    <!-- End awe-page-loading -->
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content popup-search">
+                <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i></button>
+                <div class="modal-body">
+                    <div class="input-group">
+                        <input type="text" class="form-control control-search" placeholder="Search and hit enter...">
+                        <button class="button_search" type="button">Search</button>
+                    </div><!-- /input-group -->
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+    <!-- End pushmenu -->
+    <div class="wrappage">
+        <header id="header" class="header-v1 header-v3">
+            <div id="topbar">
+                <div class="container container-ver2">
+                    <div class="inner-topbar box">
+                        <div class="float-left">
+                            <p class="acc"><img src="assets/images/icon-user-header.png" alt="icon">My Account</p>
+                            <p class="phone"><img src="assets/images/icon-phone-header.png.png" alt=""><span>070-7782-9137</span></p>
+                        </div>
+                        <div class="logo">
+                            <a href="#" title="Uno">
+                                <img src="assets/images/logo-v1.png" alt="images">
+                            </a>
+                        </div>
+                        <div class="float-right align-right">
+                            <div class="box-right">
+                                <div class="cart hover-menu">
+                                    <p class="icon-cart" title="Add to cart">
+                                        <i class="icon"></i>
+                                        <span class="cart-count">2</span>
+                                    </p>
+                                    <div class="cart-list list-menu">
+                                        <ul class="list">
+                                            <li>
+                                                <a href="#" title="" class="cart-product-image"><img src="assets/images/products/1.jpg" alt="Product"></a>
+                                                <div class="text">
+                                                    <p class="product-name">Legging Cathedral</p>
+                                                    <p class="product-price"><span class="price-old">$700.00</span><span class="price">$350.00</span></p>
+                                                    <p class="qty">QTY:01</p>
+                                                </div>
+                                                <a class="close" href="#" title="close"><i class="fa fa-times-circle"></i></a>
+                                            </li> 
+                                            <li>
+                                                <a href="#" title="" class="cart-product-image"><img src="assets/images/products/2.jpg" alt="Product"></a>
+                                                <div class="text">
+                                                    <p class="product-name">Legging Cathedral</p>
+                                                    <p class="product-price"><span class="price-old">$700.00</span><span class="price">$350.00</span></p>
+                                                    <p class="qty">QTY:01</p>
+                                                </div>
+                                                <a class="close" href="#" title="close"><i class="fa fa-times-circle"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#" title="" class="cart-product-image"><img src="assets/images/products/3.jpg" alt="Product"></a>
+                                                <div class="text">
+                                                    <p class="product-name">Legging Cathedral</p>
+                                                    <p class="product-price"><span class="price-old">$700.00</span><span class="price">$350.00</span></p>
+                                                    <p class="qty">QTY:01</p>
+                                                </div>
+                                                <a class="close" href="#" title="close"><i class="fa fa-times-circle"></i></a>
+                                            </li>
+                                        </ul>
+                                        <p class="total"><span class="left">Total:</span> <span class="right">$1121.98</span></p>
+                                        <div class="bottom">
+                                            <a class="link-v1" href="#" title="viewcart">View Cart</a>
+                                            <a class="link-v1 rt" href="#" title="checkout">Check out</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="search dropdown" data-toggle="modal" data-target=".bs-example-modal-lg">
+                                    <i class="icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End container -->
+            </div>
+            <!-- End topbar -->
+            <div class="header-top">
+                    <div class="container container-ver2">
+                    <div class="box">
+                        <p class="icon-menu-mobile"><i class="fa fa-bars" ></i></p>
+                        <div class="logo-mobile"><a href="#" title="Xanadu"><img src="assets/images/logo-v1.png" alt="Xanadu-Logo"></a></div>
+                        <nav class="mega-menu">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                          <ul class="nav navbar-nav" id="navbar">
+                            <li class="level1 active dropdown"><a href="#" title="Home">Home</a>
+                              
+                            </li>
+                              <li class="level1"><a href="#" title="About us">About us</a></li>
+                              <li class="level1"><a href="#" title="Store">Store</a></li>
+                            <li class="level1 dropdown">
+                                <a href="#" title="men">Page</a>
+                                <div class="sub-menu sub-menu-v2 dropdown-menu">
+                                <div class="top-sub-menu">
+                                    <img src="assets/images/top-submenu1.jpg" alt="images">
+                                </div>
+                                 
+                            </li>
+                            <li class="level1"><a href="#" title="collections">Blog</a></li>
+                            <li class="level1"><a href="#" title="Contact us">Contact us</a></li>
+                          </ul>
+                        </nav>
+                    </div>                
+                    </div>
+                    <!-- End container -->
+                </div>
+                <!-- End header-top -->
